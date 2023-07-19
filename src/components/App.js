b67423c6c811ecdb3e958c2f4472ccee0f68838b
peterkomo <mwangipeterkomo@gmail.com>
2023-07-19 20:52:49 +0300

    tried

--- src/components/App.js
+++ src/components/App.js
@@ -10,9 +10,13 @@ console.log(user);
 function App() {
   return (
     <div>
+
       <NavBar />
-      <Home />
-      <About />
+      <Home user={user}/>
+      <About 
+      bio={user.bio}
+      github={user.links.github}
+      linkedin={user.links.linkedin}/>
     </div>
   );
 }
